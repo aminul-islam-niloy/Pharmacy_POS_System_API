bcc0aefb7dfbff95d2898ffce581b298fb6b6d06
aminul-islam-niloy <niloyftaminul@gmail.com>
2025-01-27 18:08:01 +0600

    Create Order Controller for save Orders

---
+++ Pharmacy_POS_System_API/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy_POS_System_API.Data;
+using Pharmacy_POS_System_API.Models;
+
+namespace Pharmacy_POS_System_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PlaceOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
+    }
+}
